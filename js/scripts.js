44195c8ba9f3e6fd0a4a19a90c4b82b82ccb9817
Alexander Jacks <alexanderjacks@Alexanders-MacBook-Air.local>
2016-06-03 11:27:36 -0700

    add form values to JS and set up console debugging for logic branching

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,34 @@ $(document).ready(function() {
     event.preventDefault();
 
     var howRemote = 0;
+    howRemote += grabRemote($("#bed").val())
+    howRemote += grabRemote($("#sleep").val())
+    howRemote += grabRemote($("#selftime").val())
+    howRemote += grabRemote($("#yourlife").val())
+    howRemote += grabRemote($("#best").val())
+
     var howLuxury = 0;
+    howLuxury += grabLuxury($("#bed").val())
+    howLuxury += grabLuxury($("#sleep").val())
+    howLuxury += grabLuxury($("#selftime").val())
+    howLuxury += grabLuxury($("#yourlife").val())
+    howLuxury += grabLuxury($("#best").val())
+
     var howNatural = 0;
+    howNatural += grabNatural($("#bed").val())
+    howNatural += grabNatural($("#sleep").val())
+    howNatural += grabNatural($("#selftime").val())
+    howNatural += grabNatural($("#yourlife").val())
+    howNatural += grabNatural($("#best").val())
 
-    if () {
 
-    } else if () {
 
-    } else if () {
-
-    } else if () {
-
+    if (howRemote > howLuxury) {
+      console.log(remote > lux)
+    } else if (howLuxury > howNatural) {
+      console.log(lux > natural & remote)
     } else {
-
+      console.log(natural > lux & remote)
     }
 
     $('.output').show();
