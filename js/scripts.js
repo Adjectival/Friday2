995be47c5d910e4aa8e858552b210cb410e04572
Alexander Jacks <alexanderjacks@Alexanders-MacBook-Air.local>
2016-06-03 12:04:32 -0700

    major revision to survey logic, now simply adds or subtracts from a single variable and delivers one of three survey results

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,20 @@
+var remoteLove = 0;
+
 $(document).ready(function() {
   $("form#whereto").submit(function(event) {
     event.preventDefault();
+    remoteLove += parseInt($("#sleep").val())
+    remoteLove += parseInt($("#selftime").val())
+    remoteLove += parseInt($("#yourlife").val())
+    remoteLove += parseInt($("#best").val())
+    remoteLove += parseInt($("#bed").val())
 
-    var howRemote = 0;
-    howRemote += grabRemote($("#bed").val())
-    howRemote += grabRemote($("#sleep").val())
-    howRemote += grabRemote($("#selftime").val())
-    howRemote += grabRemote($("#yourlife").val())
-    howRemote += grabRemote($("#best").val())
-
-    var howLuxury = 0;
-    howLuxury += grabLuxury($("#bed").val())
-    howLuxury += grabLuxury($("#sleep").val())
-    howLuxury += grabLuxury($("#selftime").val())
-    howLuxury += grabLuxury($("#yourlife").val())
-    howLuxury += grabLuxury($("#best").val())
-
-    var howNatural = 0;
-    howNatural += grabNatural($("#bed").val())
-    howNatural += grabNatural($("#sleep").val())
-    howNatural += grabNatural($("#selftime").val())
-    howNatural += grabNatural($("#yourlife").val())
-    howNatural += grabNatural($("#best").val())
-
-
-
-    if (howRemote > howLuxury) {
-      console.log(remote > lux)
-    } else if (howLuxury > howNatural) {
-      console.log(lux > natural & remote)
+    if (remoteLove >= 3) {
+      console.log("high, so Q")
+    } else if (remoteLove >= 1) {
+      console.log("med, so K")
     } else {
-      console.log(natural > lux & remote)
+      console.log("low, better go to Monaco")
     }
 
     $('.output').show();
